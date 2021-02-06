a63d0937ff871fd09474077b1c4e60ab6ec164d9
cac1837 <68131380+cac18307@users.noreply.github.com>
2021-02-05 22:43:57 -0600

    ultimos
    Se guardan ultimos cambios

--- LAB_2/LAB2_DIGITAL.X/LAB2.c
+++ LAB_2/LAB2_DIGITAL.X/LAB2.c
@@ -21,36 +21,25 @@
 #pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
 #pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
 
-#define _XTAL_FREQ  4000000
+#define _XTAL_FREQ  8000000
 
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 #include <xc.h>
-#include <stdint.h>
-#include "Dis.h"
-#include "ADC.h"
-
 //----------------------------------------------------------------------------//
 //                      Declaracion de variables                              //
 //----------------------------------------------------------------------------//
 
-int A; 
-uint16_t led;   //Es un entero corto sin signo,variable en la que guardare valor 
-int B;
-int display1;
-int display2;
-int camb;
-int k;
+int A = 0; 
+unsigned short int led; //Es un entero corto sin signo,
 
 
 //----------------------------------------------------------------------------//
 //                      Declaracion de funciones                              //
 //----------------------------------------------------------------------------//
 void led_cambio(void);
-void compare(void);
+void comparar(void);
 void display(void);
-void setup(void);
-void ADCFULL (void);
 
 
 //----------------------------------------------------------------------------//
@@ -58,18 +47,7 @@ void ADCFULL (void);
 //----------------------------------------------------------------------------//
 
 void main(void) {
-    setup();
-    FUNCION:
-        led_cambio();
-        ADCFULL();
-        display();
-        compare();
-    goto FUNCION;
-}
-
-
-void led_cambio(void){      // aumentar o disminuer el valor de la variable A
-    if (A == 1){            // si el valor de A es 1 el valor de la led aumenta
+    if (A == 1){ // si el valor de A es 1 el valor de la led aumenta
        if (led == 0){
            led = 1;
        } 
@@ -82,7 +60,7 @@ void led_cambio(void){      // aumentar o disminuer el valor de la variable A
        A=0;
        PORTA = led;
     }
-    else if (A == 2){       // si el vaalor de A es 2 el valor de la led decrece
+    else if (A == 2){ // si el vaalor de A es 2 el valor de la led decrece
         if (led == 0){
            led = 255;
        } 
@@ -101,104 +79,36 @@ void led_cambio(void){      // aumentar o disminuer el valor de la variable A
     }
 }
 
-void display(void){     // se utilizara para colocar los valores del ADC en las variables y llamamos a la libreria
-    if(B == 0){         // usara los menos significativos del ADC
-        display1 = camb & 0b00001111;
-        PORTDbits.RD1 = 0;
-        Dis(display1);
-        PORTDbits.RD2 = 1;  
-    }
-    else if (B == 1){   // usara los mas significativos del ADC
-        display2 = camb & 0b11110000;
-        display2 = display2>>4;
-        PORTDbits.RD2 = 0;
-        Dis(display2);
-        PORTDbits.RD1 = 1;
+void display(void){ // se utilizara para colocar los valores del ADC en las variables y llamamos a la libreria
+    if(B == 0){ // usara los mas significativos del ADC
+        display_1 = 
+        
     }
 }
 
-void compare(void){ // se compararan los valores del ADC y encendera la alarma si es necesario
-    if (camb > led){
-        PORTDbits.RD3 = 1;
-    }
-    else{
-        PORTDbits.RD3 = 0;
-    } 
-}
 
-void ADCFULL(void){ // solo se guarda el valor del ADC y reinicia
-    if(ADCON0bits.GO_DONE == 0){ //conversion finalizada
-        camb = ADRESH;
-        ADCON0bits.GO_DONE = 1; // conversion en progreso 
-        GO = 0;
-    }
-}
-//----------------------------------------------------------------------------//
-//                              Interrupciones                                 //
-//----------------------------------------------------------------------------//
-
-void _interrupt(); isr(void){
-    if (INTCONbits.RBIF == 1){ // interrupcion del puerto B se cambia el valor de A
-        if (PORTBbits.RB7 == 1){
-            A = 1;
-        }
-        if (PORTBbits.RB3 == 0){
-            A = 2;
-        }
-    INTCONbits.RBIF = 0;    
-    }
-    else if (INTCONbits.T0IF == 1){ // interrupcion del timer 0 para multiplexeado de los displays
-        if(B == 1){
-            B = 0;
-            INTCONbits.T0IF = 0;
-            TMR0 = 6;
-        }
-        else {
-            B = 1;
-            INTCONbits.T0IF = 0;
-            TMR0 = 6;
-        }    
-    }
-    else if (PIR1bits.ADIF == 1){ // interrupcion de ADC para indicar si ha terminado de convertir
-        camb = ADRESH;
-        ADCON0bits.GO_DONE = 1; 
-        PIR1bits.ADIF = 0;
-    }
-}
-
-//configuracion de puertos 
 
 void setup(void){
-    ANSEL = 0b00100000;   // se coloco como la entrada analogica del ANS para ADC
+    ANSEL = 0;        // Establecemos los puertos
     ANSELH = 0;
     
-    TRISA = 0;
-    PORTA = 0;          // puertos configurados como salidas
-     
+    PORTA = 0;
+    
+    TRISE = 0;      //Establecemos el puerto E como salida y se limpia
+    PORTE = 0;      //SEMAFORO
+    
     TRISC = 0;
-    PORTC = 0;      
+    PORTC = 0;      //JUGADOR 1
     
     TRISD= 0;
-    PORTD = 0;      
+    PORTD = 0;      //JUGAROR 2
     
-    TRISB = 0b11111111;  // puerto B se coloco como entrada con interrupcion   
-    PORTB = 0;              
+    TRISB = 0B00001111;     
+    PORTB = 0;              // BOTONES DE JUGADOR Y RST
     
-    OPTION_REGbits.T0CS = 0; //conf del timer 0 con interrupcion de 0.0005 seg
-    OPTION_REGbits.T0SE = 0;
-    OPTION_REGbits.PSA = 0;
-    OPTION_REGbits.PS2 = 0;
-    OPTION_REGbits.PS1 = 0;
-    OPTION_REGbits.PS0 = 0;
-    TMR0 = 6;
-    INTCONbits.PEIE = 1; // habilitacion perifica de interrupciones
-    INTCONbits.T0IE = 1;
-    INTCONbits.RBIE = 1; //bandera de interrupciones
-    INTCONbits.T0IF = 0;
-    INTCONbits.RBIF = 0;
-    INTCONbits.GIE = 1; //habilitacion global de interrupciones
-    PIE1bits.ADIE = 1; //habilita interrupcion de conversion a/d
-    PIR1bits.ADIF = 0; // es una bandera de interrupcion 0 converion A/D no completada 
-    ADC(); // ADC desde libreria
-    led = 0;
+  
+}
+
+void led_cambio(void){ // aumentar o disminuer el valor de la variable A
+    
 }
